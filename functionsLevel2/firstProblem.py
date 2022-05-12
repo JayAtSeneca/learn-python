4a33ff464765cb83566019ba2419ebe4469578e5
jay chaudhari <jpchaudhari@myseneca.ca>
2022-05-12 18:15:30 -0400

    Fixed Mistakes

--- functionsLevel2/firstProblem.py
+++ functionsLevel2/firstProblem.py
@@ -1,6 +1,6 @@
 def has_33(myList):
     for num in range(0,len(myList) - 1):
-        if num == 3 and num + 1 == 3:
+        if myList[num] == 3 and myList[num + 1] == 3:
             return True
         else:
             pass
