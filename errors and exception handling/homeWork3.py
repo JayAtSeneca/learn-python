1d331a75e00e8e5a68a182fbcfe3606154c0617d
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-06-01 01:23:42 -0400

    this one too

--- errors and exception handling/homeWork3.py
+++ errors and exception handling/homeWork3.py
@@ -4,6 +4,7 @@ def ask():
             result = int(input('Please enter a number:'))
         except:
             print('An error occurred')
+            continue
         else:
             break
     print('Thank you the number is:',result**2)
